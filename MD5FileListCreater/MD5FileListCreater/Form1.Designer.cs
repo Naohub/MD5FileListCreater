b6fe43af21efed24e83276ae98e5184d2da5f874
Naohub <gt.naoki@gmail.com>
2021-12-21 18:18:54 +0900

    Add files via upload

---
+++ MD5FileListCreater/MD5FileListCreater/Form1.Designer.cs
@@ -0,0 +1,93 @@
+﻿namespace MD5FileListCreater
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージ リソースが破棄される場合 true、破棄されない場合は false です。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SelectFilesButton = new System.Windows.Forms.Button();
+            this.CreateButton = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.lstFiles = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // SelectFilesButton
+            // 
+            this.SelectFilesButton.Location = new System.Drawing.Point(12, 12);
+            this.SelectFilesButton.Name = "SelectFilesButton";
+            this.SelectFilesButton.Size = new System.Drawing.Size(75, 23);
+            this.SelectFilesButton.TabIndex = 0;
+            this.SelectFilesButton.Text = "ファイル選択";
+            this.SelectFilesButton.UseVisualStyleBackColor = true;
+            this.SelectFilesButton.Click += new System.EventHandler(this.SelectFilesButton_Click);
+            // 
+            // CreateButton
+            // 
+            this.CreateButton.Location = new System.Drawing.Point(197, 226);
+            this.CreateButton.Name = "CreateButton";
+            this.CreateButton.Size = new System.Drawing.Size(75, 23);
+            this.CreateButton.TabIndex = 2;
+            this.CreateButton.Text = "出力";
+            this.CreateButton.UseVisualStyleBackColor = true;
+            this.CreateButton.Click += new System.EventHandler(this.CreateButton_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            this.openFileDialog1.Multiselect = true;
+            // 
+            // lstFiles
+            // 
+            this.lstFiles.FormattingEnabled = true;
+            this.lstFiles.ItemHeight = 12;
+            this.lstFiles.Location = new System.Drawing.Point(13, 42);
+            this.lstFiles.Name = "lstFiles";
+            this.lstFiles.Size = new System.Drawing.Size(259, 172);
+            this.lstFiles.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.lstFiles);
+            this.Controls.Add(this.CreateButton);
+            this.Controls.Add(this.SelectFilesButton);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button SelectFilesButton;
+        private System.Windows.Forms.Button CreateButton;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.ListBox lstFiles;
+    }
+}
+
---
+++ MD5FileListCreater/MD5FileListCreater/Form1.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.IO;
+
+namespace MD5FileListCreater
+{
+    public partial class Form1 : Form
+    {
+
+        string _selectedPath = "";
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void SelectFilesButton_Click(object sender, EventArgs e)
+        {
+            // ダイアログボックスの表示
+            if (openFileDialog1.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                // リストボックスの初期化
+                lstFiles.Items.Clear();
+
+                // 選択されたファイルをテキストボックスに表示する
+                foreach (string strFilePath in openFileDialog1.FileNames)
+                {
+                    // ファイルパスからファイル名を取得
+                    string strFileName = System.IO.Path.GetFileName(strFilePath);
+
+                    //パス取得
+                    _selectedPath = System.IO.Path.GetDirectoryName(strFilePath);
+
+                    // リストボックスにファイル名を表示
+                    lstFiles.Items.Add(strFileName);
+                }
+            }
+        }
+
+        private void CreateButton_Click(object sender, EventArgs e)
+        {
+
+            StringBuilder sb = new StringBuilder();
+            sb.Clear();
+
+            foreach (string str in lstFiles.Items) {
+ 
+                sb.Append( CalcMD5(_selectedPath,str));
+                sb.Append("  ");
+                sb.Append(str);
+                sb.Append("\n");
+            }
+
+            OutputFile(sb.ToString());
+        }
+
+        private String CalcMD5(string dirName, string fileName)
+        {
+
+            //MD5ハッシュ値を計算するファイル
+
+            //ファイルを開く
+            System.IO.FileStream fs = new System.IO.FileStream(
+                System.IO.Path.Combine(dirName, fileName),
+                System.IO.FileMode.Open,
+                System.IO.FileAccess.Read,
+                System.IO.FileShare.Read);
+
+            //MD5CryptoServiceProviderオブジェクトを作成
+            System.Security.Cryptography.MD5CryptoServiceProvider md5 =
+                new System.Security.Cryptography.MD5CryptoServiceProvider();
+
+            //ハッシュ値を計算する
+            byte[] bs = md5.ComputeHash(fs);
+
+            //リソースを解放する
+            md5.Clear();
+            //ファイルを閉じる
+            fs.Close();
+
+            //byte型配列を16進数の文字列に変換
+            System.Text.StringBuilder result = new System.Text.StringBuilder();
+            foreach (byte b in bs)
+            {
+                result.Append(b.ToString("x2"));
+            }
+            //ここの部分は次のようにもできる
+            //string result = BitConverter.ToString(bs).ToLower().Replace("-","");
+
+            //結果を表示
+            return result.ToString();
+        }
+
+        private void OutputFile(string md5) {
+
+            SaveFileDialog dlg = new SaveFileDialog();
+            dlg.AddExtension = true; // ユーザーが拡張子を省略したときに、自動的に拡張子を付けるか。規定値はtrue。
+            dlg.CheckFileExists = false; // ユーザーが存在しないファイルを指定したときに、警告するか。規定値はfalse。
+            dlg.CheckPathExists = true; // ユーザーが存在しないパスを指定したときに、警告するか。規定値はtrue。
+            dlg.CreatePrompt = false; // ユーザーが存在しないファイルを指定したときに、作成の許可を求めるか。規定値はfalse。
+            dlg.DefaultExt = string.Empty; // ダイアログに表示するファイルの拡張子。規定値はEmpty。
+            dlg.DereferenceLinks = false; // ショートカットが参照先を返す場合はtrue。リンクファイルを返す場合はfalse。規定値はfalse。
+            dlg.FileName = "calcmd5.txt"; // 選択されたファイルのフルパス。
+            dlg.Filter = "テキスト文書|*.txt"; // ダイアログで表示するファイルの種類のフィルタを指定する文字列。
+            dlg.FilterIndex = 1; // 選択されたFilterのインデックス。規定値は1。
+            dlg.InitialDirectory = _selectedPath; // ダイアログの初期ディレクトリ。規定値はEmpty。
+            dlg.OverwritePrompt = true; // 存在するファイルを指定したときに、警告するか。規定値はtrue。
+            dlg.Title = "保存するファイル名を入力してください。"; // ダイアログのタイトル。
+            dlg.ValidateNames = true; // ファイル名がWin32に適合するか検査するかどうか。規定値はfalse。
+
+            if (DialogResult.OK == dlg.ShowDialog())
+            {
+
+                string filename = dlg.FileName;
+
+                Encoding enc = Encoding.GetEncoding("shift_jis");
+                try
+                {
+                    File.WriteAllText(filename, md5, enc);
+                    MessageBox.Show("出力完了");
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message);
+                    return;
+                }
+            }
+        }
+    }
+}
